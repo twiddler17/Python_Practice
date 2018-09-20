91c79dec10a36b6cf7dff13d90302654c0017b9e
twiddler17 <twiddler17@gmail.com>
2018-09-20 16:41:02 -0500

    num_times commit

--- practicepython_org/CharacterInput.py
+++ practicepython_org/CharacterInput.py
@@ -3,8 +3,10 @@
 #   File Name:          CharacterInput.py
 #   Author:             George Sinitsyn (github: twiddler17)
 #   Date create:        9/19/2018
-#   Date last modified: 9/19/2018
+#   Date last modified: 9/20/2018
 #   Python Version:     3.6.1
+#
+#   Update: Modified to display num_times
 #
 ############################################################
 
@@ -15,29 +17,37 @@ from datetime import datetime
 
 name_empty = True
 age_empty = True
+num_empty = True
 
-while name_empty == True:
-    print("What's your name?:", end = " ")
-    name = input()
+while name_empty:
+    name = input("What's your name?: ")
     if not name:
-        print("Please enter your name:")
+        pass
     else:
         name_empty = False
 
-while age_empty == True:
-    print("What's your age?:", end = " ")
-    age = int(input())
+while age_empty:
+    age = int(input("What's your age?: "))
     if not age:
-        print("Please enter your age:")
+        pass
     else:
         age_empty = False
 
+while num_empty:
+    num_times = int(input("Number of times to be displayed: "))
+    if not num_times:
+        pass
+    else:
+        num_empty = False
+
 current_year = datetime.now().year
 
-if age == 100:
-    print(name + ", you are already 100 years old.")
-else:
-    date_old = str(current_year + 100 - age)
-    print(name + ", you will be 100 years old in " + date_old + ".")
+while num_times > 0:
+    if age == 100:
+        print(name + ", you are already 100 years old.")
+    else:
+        date_old = str(current_year + 100 - age)
+        print(name + ", you will be 100 years old in " + date_old + ".")
+    num_times -= 1
 
 #End
