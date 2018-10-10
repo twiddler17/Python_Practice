33d1f584ab3a5616e891b6eac0f6fd9093bb4ff6
twiddler17 <twiddler17@gmail.com>
2018-10-10 18:55:55 -0500

    First Commit

---
+++ practicepython_org/list_overlap.py
@@ -0,0 +1,24 @@
+############################################################
+#
+#   File Name:          list_overlap.py
+#   Author:             George Sinitsyn (github: twiddler17)
+#   Date create:        10/10/2018
+#   Date last modified: 10/10/2018
+#   Python Version:     3.6.1
+#
+#   Update: First commit.
+#
+############################################################
+
+#Begin
+
+a = [1, 2, 3, 3]
+b = [2, 3, 4, 5]
+c = []
+
+for i in a:
+    if i in b: c.append(i)
+
+print(c)
+
+#End
