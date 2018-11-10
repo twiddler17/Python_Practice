ef1a9bcb0e215bf97528f23407308466b5116a4e
twiddler17 <twiddler17@gmail.com>
2018-11-09 21:42:30 -0600

    First and Final Commit

---
+++ practicepython_org/list_comprehensions.py
@@ -0,0 +1,21 @@
+############################################################
+#
+#   File Name:          character_input.py
+#   Author:             George Sinitsyn (github: twiddler17)
+#   Date create:        11/7/2018
+#   Date last modified: 11/7/2018
+#   Python Version:     3.6.1
+#
+#   Update: First Commit.
+#
+############################################################
+
+#Begin
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+final =  [i for i in a if (i % 2) == 0]
+
+print("Final: ", final)
+
+#End
